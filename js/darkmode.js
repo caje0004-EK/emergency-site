802879d6f3f0c6ac3d0b8d282894913b18b15228
Caroline Jensen <caje0004@stud.ek.dk>
2025-11-20 11:52:12 +0100

    darkmode

---
+++ js/darkmode.js
@@ -0,0 +1,8 @@
+const html = document.querySelector("html");
+const btn = document.querySelector("#toggle");
+
+function toggleTheme() {
+  html.classList.toggle('dark');
+}
+
+btn.addEventListener('click', toggleTheme);
